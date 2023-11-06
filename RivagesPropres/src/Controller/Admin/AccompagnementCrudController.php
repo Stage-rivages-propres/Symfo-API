08d3f7014218dfa3e6d6e8dac951a3ffbe9b4c00
Karl-Gavois <Gavois.k@hotmail.com>
2023-11-06 11:33:12 +0100

    Vich Easy admin

---
+++ RivagesPropres/src/Controller/Admin/AccompagnementCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Accompagnement;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+
+class AccompagnementCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Accompagnement::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            TextField::new('title'),
+            TextEditorField::new('description'),
+            TextField::new('imageFile') ->setFormType(VichFileType::class)->onlyWhenCreating(),
+            TextField::new('imageFile') ->setFormType(VichFileType::class)->onlyWhenUpdating(),
+            ImageField::new('ImageName') ->setBasePath('/uploads/images')->onlyonindex(),
+        ];
+    }
+}
