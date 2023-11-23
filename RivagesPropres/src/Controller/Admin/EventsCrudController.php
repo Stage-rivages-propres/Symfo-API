a628d2ea79a4d718dd81ce5f22e8dbd3f916a339
Karl-Gavois <Gavois.k@hotmail.com>
2023-11-23 13:48:07 +0100

    Event images

--- RivagesPropres/src/Controller/Admin/EventsCrudController.php
+++ RivagesPropres/src/Controller/Admin/EventsCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class EventsCrudController extends AbstractCrudController
 {
@@ -15,14 +17,14 @@ class EventsCrudController extends AbstractCrudController
         return Events::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('titre'),
+            TextField::new('texte'),
+            TextField::new('imageFile') ->setFormType(VichFileType::class)->onlyWhenCreating(),
+            TextField::new('imageFile') ->setFormType(VichFileType::class)->onlyWhenUpdating(),
+            ImageField::new('ImageName') ->setBasePath('/uploads/images')->onlyonindex(),
         ];
     }
-    */
 }
