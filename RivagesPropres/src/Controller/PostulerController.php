7fa86ae440b8ce37552943a871c0fc055284bdff
Karl-Gavois <Gavois.k@hotmail.com>
2024-01-03 23:27:00 +0100

    EZ POSTULER

--- RivagesPropres/src/Controller/PostulerController.php
+++ RivagesPropres/src/Controller/PostulerController.php
@@ -23,27 +23,35 @@ class PostulerController extends AbstractController
     #[Route('/postuler')]
     public function sendEmail(Request $request): Response
     {
-        // Récup données
-        $objet = $request->request->get('objet');
-        $mail = $request->request->get('mail');
-        $message = $request->request->get('message');
-        $prenom = $request->request->get('prenom');
-        $nom = $request->request->get('nom');
-
+            // Récup données
+        $data = json_decode($request->getContent());
+        // dd($data);
+        $prenom = $data->prenom;
+        $nom = $data->nom;
+        $mail = $data->mail;
+        $cvFile = $data->cv;
+        $cafFile = $data->caf;
+        $dob = $data->dob;
+        
         $email = (new TemplatedEmail())
-            ->from('gavoiskarl@gmail.com')
-            ->to('gavois.karl@gavois-k.fr')
-            ->htmlTemplate('emails/postuler.html.twig')
-            // ->cc('cc@example.com')
-            // ->bcc('bcc@example.com')
-            // ->replyTo('fabien@example.com')
-            // ->priority(Email::PRIORITY_HIGH)
-            ->subject("Nouveau message de la part de : $prenom $nom")
-            ->text('Je suis un texte')
-            ->html('<p>See Twig integration for better HTML integration!</p>'); 
+        ->from('gavoiskarl@gmail.com')
+        ->to('gavois.karl@gavois-k.fr')
+        ->subject("Nouvel envoi de candidature !")
+        ->htmlTemplate('emails/postuler.html.twig')
+        ->context([
+            'nom' => $nom,
+            'prenom' => $prenom,
+            'cv' => $cvFile,
+            'mail' => $mail,
+            'caf' => $cafFile,
+            'dob' => $dob
+        ])
+        ->attach($cvFile)
+        ->attach($cafFile);
 
         $this->mailer->send($email);
 
         return new JsonResponse(['status' => 'success']);
+
     }
 }
